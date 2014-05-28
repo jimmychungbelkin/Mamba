313c6f3c0909d36ac5431fa4b55415ed74f2dfb6
thienpc <thien.pham@veriksystems.com>
2014-05-28 12:22:12 +0700

    check to clean v4.1 old data

--- Mamba/barrier_breaker/tmp/.host.mk
+++
@@ -1,9 +0,0 @@
-HOST_OS:=Linux
-HOST_ARCH:=i686
-GNU_HOST_NAME:=i686-linux-gnu
-TAR:=/bin/tar
-FIND:=/usr/bin/find
-BASH:=/bin/bash
-FIND_L=/usr/bin/find -L $(1)
-PATCH:=/usr/bin/patch
-PYTHON:=/usr/bin/python2.7
