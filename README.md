5dbfcead4f5f080f37bdb0f2f34f1dcfcb35df0e
thienpc <thien.pham@veriksystems.com>
2014-07-23 15:59:58 +0700

    v3.9 is uploaded, containing dl folder

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Added by ThienPham (thien.pham@veriksystems.com) - 23jul14
+branch attitude_adjustment is created to keep source code V3.9
+
+
+
 Added by ThienPham (thien.pham@veriksystems.com) - 06may14
 
 branch attitude_adjustment is created to keep source code V3.5
---
+++ attitude_adjustment/feeds/routing.tmp/info/.files-packageinfo.mk
@@ -0,0 +1,7 @@
+$(eval $(call PackageDir,babeld,babeld))
+$(eval $(call PackageDir,luci-app-bmx6,luci-app-bmx6))
+$(eval $(call PackageDir,bmxd,bmxd))
+$(eval $(call PackageDir,olsrd,olsrd))
+$(eval $(call PackageDir,bmx6,bmx6))
+$(eval $(call PackageDir,batmand,batmand))
+$(eval $(call PackageDir,batman-adv,batman-adv))
